78edcb1ae60c6119f556bed6cb68c300112c0ba7
mohamed.ayman1 <mohamedayman@hotmail.com>
2023-02-01 20:23:31 +0200

    Trying Flask Framework

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
+# import os
+# import docx
+#
+#
+# def create_word_files_from_template(directory, file_names, template_file):
+#     if not os.path.exists(directory):
+#         os.makedirs(directory)
+#
+#     template = docx.Document(template_file)
+#
+#     for file_name in file_names:
+#         file_path = os.path.join(directory, file_name + ".docx")
+#         doc = template.clone()
+#         doc.save(file_path)
+#
+#
+# # Example usage
+# directory = "word_files"
+# file_names = ["file1", "file2", "file3"]
+# template_file = "template.docx"
+#
+# create_word_files_from_template(directory, file_names, template_file)
+from flask import Flask, request
 import os
 import docx
 
+app = Flask(__name__)
 
-def create_word_files_from_template(directory, file_names, template_file):
+
+@app.route("/create_word_files", methods=["POST"])
+def create_word_files():
+    directory = request.form.get("directory")
+    file_names = request.form.getlist("file_names")
+    template_file = request.form.get("template_file")
+
     if not os.path.exists(directory):
         os.makedirs(directory)
 
@@ -13,10 +43,8 @@ def create_word_files_from_template(directory, file_names, template_file):
         doc = template.clone()
         doc.save(file_path)
 
+    return "Word files created successfully!"
 
-# Example usage
-directory = "word_files"
-file_names = ["file1", "file2", "file3"]
-template_file = "template.docx"
 
-create_word_files_from_template(directory, file_names, template_file)
+if __name__ == "__main__":
+    app.run()
